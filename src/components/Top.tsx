57610cf60c93834ee19238111e4de91aca3339fc
KimGiHong <68697807+KimGiHong@users.noreply.github.com>
2021-12-16 09:45:26 +0900

    Add Top

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,35 +1,60 @@
+import { useRouter } from "next/dist/client/router";
 import styled from "styled-components";
 
 const TopBg = styled.div`
+  position: fixed;
   display: flex;
 	justify-content: center;
   align-items: center;
   width: 100%;
-  height: 5vh;
+  height: 3.5vh;
   background-color: #FFFFFF;
-  color: ${(props) => props.theme.MAIN_BLUE};
+  color: ${(props) => props.theme.MAIN_LIGHT_BLUE};
+  transition: opacity 0.5s;
 `;
 
 const TopWrapper = styled.div`
+  display: flex;
+	justify-content: space-between;
+  align-items: center;
   width: 40%;
-  background-color: black;
 `;
 
 const TopLogo = styled.img`
-  width: 35px;
-  height: 35px;
+  width: 30px;
+  height: 30px;
   display: block;
 `;
 
+const Menu = styled.div`
+  width: 70px;
+  height: 20x;
+  text-align: center;
+  font-size: 14px;
+`;
+
 export default function Top() {
-    return (
-      <>
-        <TopBg>
-          <TopWrapper>
-            <TopLogo src="/image/TopLogo.png"/>
-            
-          </TopWrapper>
-        </TopBg>
-      </>
-    );
+  const router = useRouter();
+
+  return (
+    <>
+      <TopBg id="TopBar">
+        <TopWrapper>
+          <TopLogo src="/image/TopLogo.png"/>
+          <Menu onClick={() => {
+            router.push("/");
+          }}>메인</Menu>
+          <Menu onClick={() => {
+            router.push("/club");
+          }}>동아리</Menu>
+          <Menu onClick={() => {
+            router.push("/major");
+          }}>학과</Menu>
+          <Menu onClick={() => {
+            router.push("/project");
+          }}>프로젝트</Menu>
+        </TopWrapper>
+      </TopBg>
+    </>
+  );
 } 
